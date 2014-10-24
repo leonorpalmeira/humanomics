07d06d3cad699620c143e2e723ae3cf50783441b
Leonor Palmeira <leonor.palmeira@ulg.ac.be>
2014-10-24 13:22:56 +0200

    Minor changes

--- Queue/pipeline.bash
+++ Queue/pipeline.bash
@@ -2,7 +2,7 @@
 # -*- coding: utf-8 -*-                                                                     
 #
 #SBATCH --job-name=Humanomics
-#SBATCH --time=48:00:00
+#SBATCH --time=2-00:00:00
 #SBATCH --mail-user=leonor.palmeira@gmail.com 
 #SBATCH --mail-type=END
 #SBATCH --output=/scratch/ulg/genan/palmeira/tmp/slurm-Humanomics-%j.out
@@ -13,12 +13,12 @@ ncores=4
 BIN=$GLOBALSCRATCH/bin
 JAVAcustom=$BIN"/java-1.7.0_25 -Xmx"$mem"g -XX:ParallelGCThreads="$ncores" -jar"
 GATK=$GLOBALSCRATCH"/src/Queue-3.2-2"
-Rscript=$HOME"/bin/Rscript-3.1.1"
+#Rscript=$HOME"/bin/Rscript-3.1.1"
 #input=$GLOBALSCRATCH"/work/rodrigo/R120807_MG_L004_sorted_RG_rmd_indelreal_BQSR.bam"
-#scalascript=$HOME"/scripts/work/rodrigo/gatk.qpp.scala"
+#queuescript=$HOME"/scripts/work/rodrigo/gatk.qpp.scala"
 echo "============== Started =================="
 echo `date`
-cd $GLOBALSCRATCH"/work/rodrigo/map/"
+cd $GLOBALSCRATCH"/work/testing-humanomics/Queue" #"work/rodrigo/map/"
 # NOTE to self:
 #  Unsupported option: --cpus-per-task
 
@@ -27,23 +27,23 @@ cd $GLOBALSCRATCH"/work/rodrigo/map/"
 # samstat
 # bam
 # .qlog # pour le output de queue
-scalascript=$HOME"/scripts/humanomics/scala/HumanomicsPipeline.scala"
+queuescript=$HOME"/scripts/humanomics/Queue/HumanomicsPipeline.scala"
 jobpars=""
-inputdir=$GLOBALSCRATCH"/work/rodrigo/map/"
+inputdir=$GLOBALSCRATCH"/work/testing-humanomics/Queue/raw-data"
 #    --input_file_1 /scratch/ulg/genan/palmeira/work/rodrigo/map/ISDBM376558_1_small.fastq.gz \
 #    --input_file_2 /scratch/ulg/genan/palmeira/work/rodrigo/map/ISDBM376558_2_small.fastq.gz \
-outputdir=$GLOBALSCRATCH"/work/rodrigo/map/"
+outputdir=$GLOBALSCRATCH"/work/testing-humanomics/Queue/res"
 ref=$GLOBALSCRATCH"/genomes/homo_sapiens/hg19/genome/HG19.fasta"
 refbwa=$GLOBALSCRATCH"/genomes/homo_sapiens/hg19/bwa_hash/HG19.fasta"
 noET=$HOME"/.ssh/leonor.palmeira_ulg.ac.be.key"
 $JAVAcustom ${GATK}"/Queue.jar" \
-    -S $scalascript \
-    -I1 $GLOBALSCRATCH"/work/rodrigo/map/ISDBM376558_1_small.fastq.gz" \
-    -I2 $GLOBALSCRATCH"/work/rodrigo/map/ISDBM376558_2_small.fastq.gz" \
-    -RG "@RG\\tID:Sample\\tSM:Sample\\tLB:Sample\\tPL:illumina\\tPU:hiseq\\tCN:null\\tDS:Instrument_HISEQ" \
-    -snps "/scratch/ulg/genan/palmeira/genomes/homo_sapiens/hg19/variation/dbsnp_137.hg19_sorted.vcf" \
-    -indels "/scratch/ulg/genan/palmeira/genomes/homo_sapiens/hg19/variation/Mills_and_1000G_gold_standard.indels.hg19_sorted.vcf" \
-    -indels "/scratch/ulg/genan/palmeira/genomes/homo_sapiens/hg19/variation/1000G_phase1.indels.hg19_sorted.vcf" \
+    -S $queuescript \
+    -I1 $inputdir"/R120807_CM_L004_R1.fastq.gz" \
+    -I2 $inputdir"/R120807_CM_L004_R2.fastq.gz" \
+    -RG "@RG\\tID:SampleCM\\tSM:SampleCM\\tLB:Sample\\tPL:illumina\\tPU:hiseq\\tCN:null\\tDS:Instrument_HISEQ" \
+    -snps $GLOBALSCRATCH"/genomes/homo_sapiens/hg19/variation/dbsnp_137.hg19_sorted.vcf" \
+    -indels $GLOBALSCRATCH"/genomes/homo_sapiens/hg19/variation/Mills_and_1000G_gold_standard.indels.hg19_sorted.vcf" \
+    -indels $GLOBALSCRATCH"/genomes/homo_sapiens/hg19/variation/1000G_phase1.indels.hg19_sorted.vcf" \
     --output_dir $outputdir \
     -l DEBUG \
     -R $ref \
@@ -80,7 +80,7 @@ echo "============== Finished ================="
 # #input=$1
 
 # java -Xmx500M -jar ${GATK}/Queue.jar \
-#     -S $scalascript \
+#     -S $queuescript \
 #     -P 5 \
 #     -R $ref \
 #     -I $input \
